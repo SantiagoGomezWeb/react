254cdf598b7b0a3c417b681b2d0030d135bd3c77
Santiago Gomez <santiago.daniel.gomez@gmail.com>
2022-09-26 14:29:38 -0300

    PreEntrega1Gomez

---
+++ src/data/navbars.js
@@ -0,0 +1,11 @@
+const topNav = [
+  { id: 1, label: "Inicio", href: "#" },
+  { id: 2, label: "Catalogo", href: "#" },
+  { id: 3, label: "Pedidos", href: "#" },
+  { id: 5, label: "Contacto", href: "#" },
+  { id: 5, label: "Contact", href: "#" },
+];
+
+export const getTopNav = () => {
+  return topNav;
+};
